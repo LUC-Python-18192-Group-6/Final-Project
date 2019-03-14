343c3ef960c61f02834c8662ac37da3f084c20cd
LUC-Python-18192-Group-6 <sgrosscurt@hotmail.com>
2019-03-14 23:04:18 +0100

    hallo

--- finaltry1.py
+++ finaltry1.py
@@ -7,13 +7,27 @@
 # resp = client.landmark_detection(image=image)
 # print(resp.landmark_annotations)
 
-def implicit():
-    from google.cloud import storage
+# def implicit():
+#     from google.cloud import storage
+#
+#     # If you don't specify credentials when constructing the client, the
+#     # client library will look for credentials in the environment.
+#     storage_client = storage.Client()
+#
+#     # Make an authenticated API request
+#     buckets = list(storage_client.list_buckets())
+#     print(buckets)
 
-    # If you don't specify credentials when constructing the client, the
-    # client library will look for credentials in the environment.
-    storage_client = storage.Client()
+# Imports the Google Cloud client library
+from google.cloud import storage
 
-    # Make an authenticated API request
-    buckets = list(storage_client.list_buckets())
-    print(buckets)
+# Instantiates a client
+storage_client = storage.Client()
+
+# The name for the new bucket
+bucket_name = 'my-new-bucket'
+
+# Creates the new bucket
+bucket = storage_client.create_bucket(bucket_name)
+
+print('Bucket {} created.'.format(bucket.name))
